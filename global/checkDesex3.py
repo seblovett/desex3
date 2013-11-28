b1745f7750ae9a4d9ff9a5f8955d0a276f6806c1
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2013-11-28 00:39:21 +0000

    Pipe stdout from magic and bim scripts to log files

--- global/checkDesex3.py
+++ global/checkDesex3.py
@@ -4,16 +4,17 @@ import sys
 import subprocess
 
 
-#              Globe Name        Top         Bottom
+#              Globe Name        Top         Bottom   Left
 globes   = [
-               ["ScanReturn"     ,"775"     ,"765"  ],
-               ["Scan"           ,"752"     ,"742"  ],
-               ["Vdd!"           ,"729"     ,"704"  ],
-               ["GND!"           ,"94"      ,"69"   ],
-               ["Clock"          ,"56"      ,"46"   ],
-               ["Test"           ,"33"      ,"23"   ],
-               ["nReset"         ,"10"      ,"0"    ]
+               ["ScanReturn"     ,"775"     ,"765"    ,"0"  ],
+               ["Scan"           ,"752"     ,"742"    ,"0"  ],
+               ["Vdd!"           ,"729"     ,"704"    ,"0"  ],
+               ["GND!"           ,"94"      ,"69"     ,"0"  ],
+               ["Clock"          ,"56"      ,"46"     ,"0"  ],
+               ["Test"           ,"33"      ,"23"     ,"0"  ],
+               ["nReset"         ,"10"      ,"0"      ,"0"  ]
             ]
+
 modules  = [
                "and2"      ,
                "buffer"    ,
@@ -40,7 +41,7 @@ modules  = [
             ]
 pfet = ["pfet","w=48","l=7"]
 nfet = ["nfet","w=30","l=7"]
-
+infoDump = "checkDesex3info"
 totalGlobals = (len(globes)*2)
 
 
@@ -53,13 +54,15 @@ print("#      Desex3 Check       #")
 print("###########################")
 
 
-if not os.path.exists("Errors"):
-   os.makedirs("Errors")
+if not os.path.exists(infoDump):
+   os.makedirs(infoDump)
+else:
+   os.system("rm "+infoDump+"/*")
 
 # Iterate modules
 globalErrors = 0;
 for module in modules:
-   infoFile = "Errors/"+module+".txt"
+   infoFile = infoDump+"/"+module+".txt"
    sys.stdout = open(infoFile, 'w')
    localErrors = 0;
    print("Testing "+module+"...\n")
@@ -93,8 +96,7 @@ for module in modules:
          numGlobals += 1
 
    if(numGlobals != totalGlobals):
-      print("      FAIL: Only "+str(numGlobals)+" global ports, there should be "+str(totalGlobals))
-      localErrors += 1
+      print("         WARNING: Found "+str(numGlobals)+" global ports, normally "+str(totalGlobals))
 
    if(localErrors == 0):
       print("         INFO: Pass global ports")
@@ -102,14 +104,27 @@ for module in modules:
 
 
 
-
    # Extract if no errors
    if(localErrors == 0):
+
+      #TODO: Can only pipe stdout to scratch file, this is bad
+
       print ("\n   Extracting from "+module+".mag to "+module+".ext")
-      os.system("magic -T c035u -dnull << EOF &> /dev/null\n:load ../"+module+"/"+module+"\n:extract\n:q")
-      print("   Creating "+module+".spice")
-      print("   Creating "+module+".sp")
-      os.system("ext2sp ../"+module+"/"+module+" &> /dev/null")
+      os.system("magic -T c035u -dnull << EOF &> "+module+"\n:load ../"+module+"/"+module+"\n:extract\n:q")
+      data = open(module,'r+')
+      for line in data.readlines():
+         print("         "+line.rstrip('\n').rstrip('\r')    )
+      print("\n   Running 'ext2sp'...")
+      os.system("ext2sp ../"+module+"/"+module+" &> "+module)
+      data = open(module,'r+')
+      for line in data.readlines():
+         print("         "+line.rstrip('\n').rstrip('\r')    )
+      print("\n   Running 'ext2svmod'...")
+      os.system("ext2svmod ../"+module+"/"+module+" &> "+module)
+      data = open(module,'r+')
+      for line in data.readlines():
+         print("         "+line.rstrip('\n').rstrip('\r')    )
+      os.system("rm "+module)
 
 
 
@@ -150,14 +165,15 @@ for module in modules:
 
    sys.stdout = open("/dev/stdout","w")
    # Display local errors
-   print
    if(localErrors == 0):
-      print("Passed "+module)
+      print(module+"... OK")
    else:
-      print("Failed "+module)
+      print(module+"... FAILED!!!!!!")
       globalErrors += localErrors
 
-   os.system("mv Errors/"+module+".txt Errors/"+str(localErrors)+"_"+module+".txt")
+
+   # Rename info file with errors
+   os.system("mv "+infoDump+"/"+module+".txt "+infoDump+"/"+module+"_"+str(localErrors)+".txt")
 
 
 
@@ -182,4 +198,4 @@ else:
       print("\n "+str(globalErrors)+" errors")
    print
 
-print("Error files held in desex3/global/Errors/\n\n")
+print("Info files held in desex3/global/"+infoDump+"\n\n")
