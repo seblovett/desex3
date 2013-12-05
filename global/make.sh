8202fe4c99e5a1f94977d02c4b15951b21d84b25
Henry Lovett <hl13g10@ecs.soton.ac.uk>
2013-12-05 23:21:42 +0000

    Added auto running of HSpice Script also calls ptython script to make the AC tables

--- global/make.sh
+++ global/make.sh
@@ -1,3 +1,29 @@
+
+#run all simulations and make tables
+cd ..
+for dir in ./*/
+do
+        dir=${dir%*/}
+        echo ${dir##*/}
+        if [ ".git" != ${dir##*/} ]; then
+        if [ "global" != ${dir##*/} ]; then
+                cd ${dir}
+                if [ "inv" != ${dir##*/} ]; then
+                        cp ../inv/inv.mag inv.mag
+                fi
+                sh simulate spice ${dir##*/}  noguiplease
+                if [ $? != 0 ]; then
+                        echo "An error has occurred with " ${dir##*/} 
+                else
+                        python ../global/mt0totable.py -i ${dir##*/}_ld.mt0 -o acresults.txt
+                fi
+                cd ..
+        fi
+        fi
+done
+
+cd global 
+
 pdflatex Databook.tex
 pdflatex Databook.tex
 pdflatex Databook.tex
